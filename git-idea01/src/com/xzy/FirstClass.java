f3dd09f7cba48325e8230d31a189eb255b5ece55
ml-start <3386441921@qq.com>
2020-01-29 22:25:51 +0800

    idea的commit

---
+++ git-idea01/src/com/xzy/FirstClass.java
@@ -0,0 +1,13 @@
+package com.xzy;
+
+import sun.awt.geom.AreaOp;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("woshifirst");
+        System.out.println("111");
+        System.out.println("222");
+
+    }
+
+}
